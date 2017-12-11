7a805d035594f8613bf47897c4cc4308387f7f08
Yaya Du <Yayadu@Yayas-MacBook-Pro.local>
2017-12-11 18:55:15 -0500

    api working

--- routes/pet.js
+++ routes/pet.js
@@ -4,7 +4,7 @@ var request = require('request');
 function trial(app){
   // app.get("api/pets/:category", function(req,res){
 
-    var url = "http://api.petfinder.com/subsystem.method?format=json&key=";
+    var url = "http://api.petfinder.com/breed.list?format=json&key=";
     var apikey = "8c46a80af9f4501e366c726a72006ad8";
     var secret = "041e4b5e005bf0fe3dce1929e5d7813b";
     //var category = req.params.category;
@@ -12,12 +12,14 @@ function trial(app){
 
 
     url += apikey;
-    url += `animal=${category}`
+    url += `&animal=${category}`
 
-    console.log(url)
+    console.log("this is the " + url)
 
     request(url, function(error, response, body){
-      console.log(JSON.parse(body));
+      var allResults = JSON.parse(body);
+      var resultArray = allResults.petfinder.breeds
+      res.json(resultArray)
     })
   // })
 
