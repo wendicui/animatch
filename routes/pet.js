5ecb79468d6e87e215bd4475fbcfc7ccc642c94c
Yaya Du <Yayadu@Yayas-MacBook-Pro.local>
2017-12-11 19:59:03 -0500

    url trial for get pet

--- routes/pet.js
+++ routes/pet.js
@@ -1,27 +1,71 @@
 var request = require('request');
+var apikey = "8c46a80af9f4501e366c726a72006ad8";
+var secret = "041e4b5e005bf0fe3dce1929e5d7813b";
+
 
 // module.exports =
 function trial(app){
-  // app.get("api/pets/:category", function(req,res){
+  //get bread list
+ // app.get("api/pets/:animal", function(req,res){
+ //    var url = "http://api.petfinder.com/breed.list?format=json&key=";
+ //    var animal = req.params.animal;
 
-    var url = "http://api.petfinder.com/breed.list?format=json&key=";
-    var apikey = "8c46a80af9f4501e366c726a72006ad8";
-    var secret = "041e4b5e005bf0fe3dce1929e5d7813b";
-    //var category = req.params.category;
-    var category = "cat";
+ //    url += apikey;
+ //    url += `&animal=${animal}`
+ //
+ //    console.log("this is the " + url)
+ //
+ //    request(url, function(error, response, body){
+ //      var allResults = JSON.parse(body);
+ //      var resultArray = allResults.petfinder.breeds
+ //      res.json(resultArray)
+ //    })
+ //
+ // })
 
+//get specified animals
+app.get("api/pets/:animal/:breed/:size/:location/:age/:sex", function(req,res){
+ var url = "http://api.petfinder.com/pet.find?format=json&key=";
+//   var param = req.params
+//   var animal = param.animal
+//   var breed = param.breed;
+//   // (S=small, M=medium, L=large, XL=extra-large)
+//   var size = param.size;
+// Baby, Young, Adult, Senio
+//   var age = param.age;
+//   //M=male, F=female
+//   var sex = param.sex;
+//   //either zipcode or state, this one is required
+//   var location = param.location
+var param = {
 
-    url += apikey;
-    url += `&animal=${category}`
+}
+  url += apikey;
 
-    console.log("this is the " + url)
+//check whether the input is made from users and add to url
+  for(key in param){
+    if(!param.key= "undefined"){
+      url += `&${key}=param[key]`
+    }
+  }
 
-    request(url, function(error, response, body){
-      var allResults = JSON.parse(body);
-      var resultArray = allResults.petfinder.breeds
-      res.json(resultArray)
-    })
-  // })
+  //
+  // url = url
+  //       + `&location=${location}`
+  //       + `&animal=${animal}`
+  //       + `&sex=${sex}`
+  //       + `&age=${age}`
+  //       + `&size=${size}`
+  //       + `&breed=${breed}`
+  //       + `&count=25`
+console.log(url)
+  request(url, function(error, response, body){
+    var results = JSON.parse(body)
+    var status = results.petfinder.header.status
+    console.log(status)
+  })
+//})
+
 
 
 }
