bcbba37723a0acf8731fc3b9c32b1fb7bf837128
geanie blanco <geanieblanco@gmail.com>
2017-12-11 20:06:59 -0500

    12-11 pre pull merge

--- public/assets/app.js
+++ public/assets/app.js
@@ -1,30 +1,40 @@
-// Initialize Firebase
-var firebase = require("firebase/app");
-require("firebase/auth");
-require("firebase/database");
+$('document').ready(funtion({
+    // Initialize Firebase
+    var firebase = require("firebase/app");
+    require("firebase/auth");
+    require("firebase/database");
 
-//Google Connect
-var provider = new firebase.auth.GoogleAuthProvider();
+    //Google Connect
+    var provider = new firebase.auth.GoogleAuthProvider();
 
-//Firebase Login
-firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // ...
-});
+    //Firebase Login
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+    });
 
-//Firebase Logout
-firebase.auth().signOut().then(function() {
-  // Sign-out successful.
-}).catch(function(error) {
-  // An error happened.
-});
+    //Firebase Logout
+    firebase.auth().signOut().then(function() {
+      // Sign-out successful.
+    }).catch(function(error) {
+      // An error happened.
+    });
 
-//Google Login
-firebase.auth().signInWithRedirect(provider);
+    //Google Login
+    firebase.auth().signInWithRedirect(provider);
 
-//Materialize Multiple SELECTOR
-$(document).ready(function() {
-  $('select').material_select();
+    //  Materialize Multiple SELECTOR
+    $(document).ready(function() {
+      $('select').material_select();
+    });
+
+    // GET INFO
+    var animalType = $("input[name='pet-search']:checked").val();
+    var currentPet = $("input[name='pet-group']:checked").val();
+    var children = $("input[name='child-group']:checked").val();
+
+    console.log(animalType);
+  };
 });
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.engine("handlebars", exphbs({
 }));
 app.set("view engine", "handlebars");
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.get("/", (req, res) => {
   res.render("index");
