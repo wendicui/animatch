e720da00570a4cf5d31301e3bfd6ba3b2a859840
geanie blanco <geanieblanco@gmail.com>
2017-12-11 20:34:51 -0500

    merge conflicts resolutions

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
 const express = require("express");
-
 const app = express();
 const port = process.env.PORT || 3000;
-
 const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const db = require("./models");
 
 app.engine("handlebars", exphbs({
   defaultLayout: "main"
@@ -49,26 +48,21 @@ app.get("/home", (req, res) => {
 });
 
 app.listen(port);
-=======
-//Dependecies
-var express = require("express");
-var bodyParser = require("body-parser");
 
-//Database
 
-var db = require("./models");
-
-//Express App
-var app = express();
-var PORT = process.env.PORT || 8080;
+//DATABASE
 db.sequelize.sync().then(function() {
   app.listen(port);
 });
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(bodyParser.json({
+  type: "application/vnd.api+json"
+}));
 app.use(express.static("public"));
 
 //Express Routes
@@ -79,10 +73,8 @@ require("./app/routes/html-routes.js")(app);
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
-<<<<<<< HEAD
 
 //Handlebars Routes
-var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({
   defaultLayout: "main"
@@ -94,6 +86,3 @@ var routes = require("./controllers/pets_controller.js");
 app.use("/", routes);
 app.use("/update", routes);
 app.use("/create", routes);
-=======
->>>>>>> master
->>>>>>> geanie-branch
