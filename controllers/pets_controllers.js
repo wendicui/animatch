6b1a6b8b0d4ed309daeb8fd7552cc1b8ff79eb16
Wendi Cui <yayadu1989@gmail.com>
2017-12-13 21:26:21 -0500

    Update pets_controllers.js

--- controllers/pets_controllers.js
+++ controllers/pets_controllers.js
@@ -42,27 +42,12 @@ router.get("/home", (req, res) => {
 
 // database route
 router.get('/', function (req, res) {
-    queries.show(function(data){
-        //console.log(data);
-        var Data = {
-            petsData: data
-        }
-        //res.render('index', {data : Data});
-        res.render('index', Data);
-    });
+    
+        res.render('index', {});
+   
 });
 
-router.post('/create', function (req, res) {
-    queries.add(req.body.item, function(data) {
-        res.redirect('/');
-    });
-});
 
-router.post('/update', function(req, res){
-  queries.add(req.body.id, function(result){
-    res.redirect('/');
-  });
-});
 
 // api router
   //get bread list
