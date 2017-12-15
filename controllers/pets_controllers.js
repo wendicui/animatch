11947d73cd8dd2f19a29c71f00159c78d277ee74
geanie blanco <geanieblanco@gmail.com>
2017-12-15 14:55:27 -0500

    more firebase / append trials

--- controllers/pets_controllers.js
+++ controllers/pets_controllers.js
@@ -1,4 +1,3 @@
-
 var express = require('express');
 var router = express.Router();
 var parser = express.json();
@@ -10,8 +9,8 @@ var apikey = "8c46a80af9f4501e366c726a72006ad8";
 var secret = "041e4b5e005bf0fe3dce1929e5d7813b";
 
 //html render
-router.get("/login",(req,res)=> {
-    res.render("login");
+router.get("/login", (req, res) => {
+  res.render("login");
 })
 
 router.get("/create-account", (req, res) => {
@@ -42,66 +41,67 @@ router.get("/home", (req, res) => {
 });
 
 
-router.get('/', function (req, res) {
-    res.render('index');
+router.get('/', function(req, res) {
+  res.render('index');
 });
 
 
 
 // api router
-  //get bread list
-  router.get("/api/pets/:animal", function(req,res){
-     var url = "http://api.petfinder.com/breed.list?format=json&key=";
-     var animal = req.params.animal;
+//get bread list
+router.get("/api/pets/:animal", function(req, res) {
+  var url = "http://api.petfinder.com/breed.list?format=json&key=";
+  var animal = req.params.animal;
 
-     url += apikey;
-     url += `&animal=${animal}`
+  url += apikey;
+  url += `&animal=${animal}`
 
-     //console.log("this is the " + url)
+  //console.log("this is the " + url)
 
-     request(url, function(error, response, body){
-       var allResults = JSON.parse(body);
-       var resultArray = allResults.petfinder.breeds
-       res.json(resultArray)
-     })
-
+  request(url, function(error, response, body) {
+    var allResults = JSON.parse(body);
+    var resultArray = allResults.petfinder.breeds
+    res.json(resultArray)
   })
 
-  //return specific animals
-  router.get("/api/pets/:animal/:breed/:size/:location/:age/:sex", function(req,res){
-   var url = "http://api.petfinder.com/pet.find?format=json&key=";
+})
 
-    url += apikey;
-    url += '&count=6'
-    //check whether the input is made from users and add to url
-    for(var key in param){
-      if(param[key] != "undefined"){
-        url += `&${key}=${param[key]}`
-      }
+//return specific animals
+router.get("/api/pets/:animal/:breed/:size/:location/:age/:sex", function(req, res) {
+  var url = "http://api.petfinder.com/pet.find?format=json&key=";
+
+  url += apikey;
+  url += '&count=6'
+  //check whether the input is made from users and add to url
+  for (var key in param) {
+    if (param[key] != "undefined") {
+      url += `&${key}=${param[key]}`
     }
+  }
 
-    request(url, function(error, response, body){
-      var results = JSON.parse(body)
-      var status = results.petfinder.header.status
-      var list = results.petfinder.pets
-      console.log(list);
-      res.json(list);
-    })
+  request(url, function(error, response, body) {
+    var results = JSON.parse(body)
+    var status = results.petfinder.header.status
+    var list = results.petfinder.pets
+    console.log(list);
+    res.json(list);
   })
+})
 
 // //get animal infor by id
-  router.get("/api/animal/:id",function(req,res){
-    var url = "http://api.petfinder.com/pet.get?format=json&key=";
-    var id = req.parmas.id
-     url += apikey;
-     url += `&id=${id}`
+router.get("/api/animal/:id", function(req, res) {
+  var url = "http://api.petfinder.com/pet.get?format=json&key=";
+  var id = req.parmas.id
+  url += apikey;
+  url += `&id=${id}`
 
-     request(url, function(err, response, body){
-       var results = JSON.parse(body)
-       var animal = results.petfinder.pets
-       res.json(animal)
-     })
+  request(url, function(err, response, body) {
+    var results = JSON.parse(body)
+    var animal = results.petfinder.pets
+    res.json(animal)
   })
+  $("$match-container").append(results);
+});
 
 // function that grab animal infor by id
 // function getAnimalbyId(idPassedIn, Obj, i){
@@ -123,16 +123,16 @@ router.get('/', function (req, res) {
 
 //for user information
 
-  router.get("/api/user/:id", function(req,res){
-    //list all the favorate animals
-    console.log("ere")
-    queries.User.findOne({
-      where:{
-        id:req.params.id
-      },
-      include:[queries.Pet]
-    }).then(function(data){
-      //create an object to hold all the data info
+router.get("/api/user/:id", function(req, res) {
+  //list all the favorate animals
+  console.log("ere")
+  queries.User.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [queries.Pet]
+  }).then(function(data) {
+    //create an object to hold all the data info
     // var animalList = {}
     // var allAnimals = data.dataValues.Pets;
     // for (var i = 0; i < allAnimals.length; i++) {
@@ -142,24 +142,24 @@ router.get('/', function (req, res) {
     // }
     //   console.log("_____________________________")
     //   console.log(animalList)
-      res.json(data);
+    res.json(data);
 
-    })
   })
+})
 
-  //create new user file
-  router.put("/api/users", function(req,res) {
-    queries.User.create(req.body).then(function(data){
-      res.json(data);
-    })
+//create new user file
+router.put("/api/users", function(req, res) {
+  queries.User.create(req.body).then(function(data) {
+    res.json(data);
   })
+})
 
-  //create new animal
+//create new animal
 
-  router.put("/api/animals", function(req,res) {
-    queries.Pet.create(req.body).then(function(data){
-      res.json(data);
-    })
+router.put("/api/animals", function(req, res) {
+  queries.Pet.create(req.body).then(function(data) {
+    res.json(data);
   })
+})
 
 module.exports = router;
--- public/assets/quiz.js
+++ public/assets/quiz.js
@@ -1,11 +1,23 @@
-var animalType;
-var currentPet;
-var children;
-var homeType;
-var activityLevel;
-
 $(document).ready(function() {
   console.log("Connected");
+
+  var animalType;
+  var currentPet;
+  var children;
+  var homeType;
+  var activityLevel;
+  var config = {
+    apiKey: "AIzaSyDH4QI1MF7gBkE9Tt9vn6rp3JXavnLGBIs",
+    authDomain: "animatch-7be82.firebaseapp.com",
+    databaseURL: "https://animatch-7be82.firebaseio.com",
+    projectId: "animatch-7be82",
+    storageBucket: "animatch-7be82.appspot.com",
+    messagingSenderId: "250916829970"
+  };
+  firebase.initializeApp(config);
+
+  var userData = firebase.database();
+
   // GET INFO
   $('#submit-button').on("click", function() {
     event.preventDefault();
@@ -18,5 +30,14 @@ $(document).ready(function() {
     console.log(animalType);
     console.log(currentPet);
     console.log(children);
+    var quizInfo = {
+      animal: animalType,
+      pets: currentPet,
+      children: children,
+      home - type: homeType,
+      activity - level: activityLevel,
+    }
+    userData.push(quizInfo);
+    window.location.replace("/account");
   });
 });
