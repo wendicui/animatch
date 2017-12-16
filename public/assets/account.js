5a1deca0d169c01e677b1429d076b54ba25d1c78
Yaya Du <Yayadu@Yayas-MBP.fios-router.home>
2017-12-15 22:36:13 -0500

    add animal data trial

---
+++ public/assets/account.js
@@ -0,0 +1,13 @@
+<!-- Local stylesheet -->
+<link rel="stylesheet" href="/assets/stylesheets/quiz.css">
+
+<!-- JQUERY -->
+<script src="https://code.jquery.com/jquery.js"></script>
+
+<!-- Local JS -->
+<script rel="JavaScript" src="/assets/account.js"></script>
+
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+
+var name = localStorage.getItem(firstname)
