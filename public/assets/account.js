dae7cf85e8d7848166a847d4aa8df80ad5b6c3e6
Wendi Cui <wcui@igrace.com>
2017-12-19 10:59:26 -0500

    match data added

--- public/assets/account.js
+++ public/assets/account.js
@@ -5,6 +5,8 @@ var email = localStorage.getItem("email")
 //get all the matched animals
 
 $(document).ready(function() {
+  var cont1 = $(".container1")
+  var cont2 = $(".container2")
   console.log(email)
   getAnimals(email)
 
@@ -18,6 +20,38 @@ $(document).ready(function() {
 
     $.get(`/api/user/email/${email}`, function(data){
       console.log(data)
+      populate(data)
     })
   }
+
+
+  //populate data onto dom
+  function populate(data){
+  // add the first four to row 1
+    for (var i = 0; i < 4; i++) {
+      var newDiv = $('<div  class="col s3 z-depth-1" id="match-container">');
+      var img= $(' <img  id="match-picture">');
+      var name = $("<p>")
+      name.text = data[i].name.$t;
+      img.src = data[i].media;
+      newDiv.append(img);
+      newDiv.append(name);
+      cont1.append(newDiv);
+    }
+
+  // add the last four to row 2
+    for (var i = 4; i < data.length; i++) {
+      var newDiv = $('<div  class="col s3 z-depth-1" id="match-container">');
+      var img= $(' <img  id="match-picture">');
+      var name = $("<p>")
+      name.text = data[i].name.$t;
+      img.src = data[i].media;
+      newDiv.append(img);
+      newDiv.append(name);
+      cont2.append(newDiv);
+    }
+
+
+  }
+
 })
